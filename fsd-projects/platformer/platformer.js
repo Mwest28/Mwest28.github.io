aa82d99c94bee58e69d59bf36eaa244773ba59ba
Mwest28 <michael.west@livingstoncollegiate.org>
2025-08-27 17:54:39 +0000

    done with project

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,39 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
 
-
-
+    
+createPlatform(900, 700, 200, 20, "black");
+createPlatform ( 400, 600 , 400, 20)
+createPlatform ( 600, 100, 100, 20)
+createPlatform (700, 400, 100, 20 )
+createPlatform ( 350, 500, 200, 10)
+createPlatform (900,300, 200, 10)
+createPlatform (800,200, 200, 10)
+createPlatform (400,300, 200,10)
+  createPlatform (200,300, 200,10)
+  createPlatform ( 100, 200, 200,10)
+  createPlatform (400,150, 200,10)
+ 
 
     // TODO 3 - Create Collectables
 
+   createCollectable ("database",350, 550);
+   createCollectable ("database", 750, 150);
+   createCollectable ("database",250, 150);
+   
 
 
     
     // TODO 4 - Create Cannons
 
+    createCannon("top", 200, 700);
+     createCannon("right", 300, 700)
+     createCannon ("left", 500, 2000)
 
     
     
